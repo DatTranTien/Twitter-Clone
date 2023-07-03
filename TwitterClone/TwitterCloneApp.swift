a9f729894ef4391d04a8c4caa0e911d7e08a5090
TranDat <dattt@mor.com.vn>
2023-07-03 09:25:22 +0700

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Admin on 03/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
